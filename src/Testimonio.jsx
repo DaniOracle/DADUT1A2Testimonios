5dd2c2ea63ab54c1e13518ae2cc620ae8ada68d8
DaniOracle <danielpereznunez9@gmail.com>
2024-10-27 17:06:42 +0000

    Utilización de los nuevos componentes

--- src/Testimonio.jsx
+++ src/Testimonio.jsx
@@ -4,21 +4,38 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardActions from '@mui/material/CardActions';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import IconButton from '@mui/material/IconButton';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { DanielPN } from 'milibreria';
+import { MyButton } from 'milibreria';
+import { MyFavourite } from 'milibreria';
+import { MyFavouriteBorderIcon } from 'milibreria';
+import Box from '@mui/material/Box';
 
 function Testimonio({ id, imageUrl, title, subTitle, text, description }) {
 
-// Estado para controlar si el corazón está marcado o no. Se inicializa como false.
-    const [liked, setLiked] = useState(false); 
+    // Estado para controlar si el corazón está marcado o no. Se inicializa como false.
+    const [liked, setLiked] = useState(false);
+    
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const handleDialogOpen = () => {
+        setDialogOpen(true);
+    };
 
     const handleLike = () => {
-// Alterna el estado de la variable boolean "liked" entre true y false cuando se hace click en el botón
-        setLiked(!liked); 
+        // Alterna el estado de la variable boolean "liked" entre true y false cuando se hace click en el botón
+        setLiked(!liked);
+    };
+
+
+
+    const handleDialogClose = () => {
+        setDialogOpen(false);
+    };
+
+    const handleSave = () => {
+
+        setDialogOpen(false);
     };
 
     return (
@@ -38,17 +55,44 @@ function Testimonio({ id, imageUrl, title, subTitle, text, description }) {
                         <Typography gutterBottom variant="h7" component="div">
                             {subTitle}
                         </Typography>
-                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                            {text}
+                        <Typography variant="body2" sx={{ color: 'text.secondary', textAlign: "justify" }}>
+                            {text.substring(0, 100)}...
                         </Typography>
                     </CardContent>
                 </CardActionArea>
+
                 <CardActions>
-                    <IconButton onClick={handleLike}>
-                        {liked === true ? <FavoriteIcon sx={{color: 'red'}} /> : <FavoriteBorderIcon sx={{color: 'black'}} />}
-                   </IconButton>
+                    <Box sx={{ display: 'flex', width: '100%' }}>
+                        {liked ? (
+                            <MyFavourite onClick={handleLike} color="red" />
+                        ) : (
+                            <MyFavouriteBorderIcon onClick={handleLike} color="black" />
+                        )}
+                        <MyButton
+                            text={"See more"}
+                            txtcolor="black"
+                            backgroundColor="white"
+                            bordercolor="black"
+                            borderwidth="2px"
+                            hoverbackgroundcolor="lightblue"
+                            hovertxtColor="black"
+                            size="large"
+                            onClick={handleDialogOpen}
+                            sx={{ marginLeft: 'auto' }}
+                        />
+                    </Box>
                 </CardActions>
             </Card>
+
+            <DanielPN
+                open={dialogOpen}
+                onClose={handleDialogClose}
+                title={title}
+                content={[text]}
+                onSave={handleSave}
+                width="600px"
+                height="400px"
+            />
 
         </>
     )
